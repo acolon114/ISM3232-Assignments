670ea310e1e07aaefe2bd4b3fbbca6d5d2aae2b2
acolon114 <acolon114@usf.edu>
2024-09-01 18:22:59 +0000

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1: Variables and Data Types
+
+// Employee Information
+let employeeName = "Alejandra Colon";
+const employeeID = 1;
+var isActive = true;
+
+// Logging Employee to Console
+console.log ("Employee Name: " + employeeName + " | Type: " + typeof(employeeName)); 
+console.log ("Employee ID: " + employeeID + " | Type: " + typeof(employeeID)); 
+console.log ("Is Active: " + isActive + " | Type: " + typeof(isActive)); 
