b6ff50d3a3aa7071cb91e33367c7a60051c2c9a2
acolon114 <acolon114@usf.edu>
2024-09-01 18:23:38 +0000

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -9,3 +9,20 @@ var isActive = true;
 console.log ("Employee Name: " + employeeName + " | Type: " + typeof(employeeName)); 
 console.log ("Employee ID: " + employeeID + " | Type: " + typeof(employeeID)); 
 console.log ("Is Active: " + isActive + " | Type: " + typeof(isActive)); 
+
+// Task 2: Compound Data Types
+
+// Declared Array Information
+let products = ["Brush", "Towel", "Mat"];
+const productDetails = new Object();
+
+//Declared Object Information
+productDetails.name = "Brush";
+productDetails.price = 7;
+productDetails.inStock = true;
+
+// Logged Array and Object to Console
+console.log(products);
+console.log( "Product Name: " + productDetails.name 
+    + " | Product Price:" + productDetails.price 
+    + " | Product in Stock:" + productDetails.inStock);
